810b713d411c009b0b58fee42fb10f1ea7738daf
LongHaHoang <longhahoang1992@gmail.com>
2023-01-23 15:41:18 +0100

    add <main> tag to index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,9 @@ export default function HomePage() {
   return (
     <AppContainer>
       <ShopHeader />
-      <Gallery />
+      <main>
+        <Gallery />
+      </main>
     </AppContainer>
   );
 }
