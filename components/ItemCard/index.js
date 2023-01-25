9a8c6043f8db57a2abf8e45d0678208699e41e5e
LongHaHoang <longhahoang1992@gmail.com>
2023-01-25 15:48:03 +0100

    FIX: rename major components to index.js

--- components/ItemCard/index.js
+++ components/ItemCard/index.js
@@ -1,3 +1,65 @@
-import { ItemCard } from "./ItemCard";
+// Import external resources and hooks
+import styled from "styled-components";
+import { useState } from "react";
 
-export default ItemCard;
+// Import of styled components
+import * as Styled from "@/components/ItemCard/ItemCard.styled.js";
+
+// Import of SVG Icon Components
+import * as Icon from "@/components/Icons";
+
+export default function ItemCard({ product, handleCartTotal }) {
+  const [counter, setCounter] = useState(0);
+
+  function increaseCounter() {
+    const newCounter = counter === 99 ? counter : counter + 1;
+    setCounter(newCounter);
+  }
+
+  function decreaseCounter() {
+    const newCounter = counter === 0 ? counter : counter - 1;
+    setCounter(newCounter);
+  }
+
+  function addToCart() {
+    if (counter > 0) {
+      handleCartTotal(counter);
+      setCounter(0);
+    }
+  }
+
+  return (
+    <li>
+      <Styled.ArticleCard>
+        <span>
+          <Styled.Thumbnail
+            src={product.thumbnail}
+            alt="no img"
+            width={120}
+            height={150}
+          />
+        </span>
+        <span>
+          <h2>{product.title}</h2>
+          <p>{product.price}€</p>
+          <div>
+            <span>
+              <button type="button" onClick={() => decreaseCounter}>
+                <Icon.SmallMinus />
+              </button>
+              <p>{counter}</p>
+              <button type="button" onClick={() => increaseCounter}>
+                <Icon.SmallPlus />
+              </button>
+            </span>
+            <span>
+              <button type="button" onClick={() => addToCart}>
+                <Icon.SmallCart />
+              </button>
+            </span>
+          </div>
+        </span>
+      </Styled.ArticleCard>
+    </li>
+  );
+}
--- components/ShopHeader/index.js
+++ components/ShopHeader/index.js
@@ -1,3 +1,18 @@
-import { ShopHeader } from "./ShopHeader";
+import {
+  StyledHeader,
+  StyledH1,
+} from "@/components/ShopHeader/ShopHeader.styled";
 
-export default ShopHeader;
+import { SmallCart } from "@/components/Icons";
+
+export default function ShopHeader({ cartTotal }) {
+  return (
+    <StyledHeader>
+      <StyledH1>Shop Name</StyledH1>
+      <p>
+        <SmallCart />
+        <span>{cartTotal}</span>
+      </p>
+    </StyledHeader>
+  );
+}
