c9e245d37ea7abd8a65511ebdd9b0f57d8b9632a
LongHaHoang <longhahoang1992@gmail.com>
2023-01-25 01:07:55 +0100

    add counterbutton function

--- components/ItemCard/ItemCard.js
+++ components/ItemCard/ItemCard.js
@@ -1,19 +1,48 @@
-// Import of external resources
+// Import external resources and hooks
 import styled from "styled-components";
+import { useState } from "react";
 
 // Import of styled components
 import * as Styled from "@/components/ItemCard/ItemCard.styled.js";
 
-// Import of SVG Components
+// Import of SVG Icon Components
 import * as Icon from "@/components/Icons";
 
 export function ItemCard({ product }) {
+  const [counter, setCounter] = useState("");
+
+  function increaseCounter() {
+    console.log("You clicked increasedCounter");
+    const newCounter = counter === 99 ? Number(counter) : Number(counter) + 1;
+    setCounter(newCounter);
+  }
+
+  function decreaseCounter() {
+    console.log("You clicked decreasedCounter");
+    const newCounter =
+      counter === 0 || counter === "" ? counter : Number(counter) - 1;
+    setCounter(newCounter);
+  }
+
+  function onChange(event) {
+    const validDigit = new RegExp("^[0-9]{0,1}");
+    if (event.target.value.length <= 2) {
+      setCounter(event.target.value);
+    } else {
+      setCounter("");
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <li>
       <Styled.ArticleCard>
         <span>
           <Styled.Thumbnail
-            src={product.images[product.images.length - 1]}
+            src={product.thumbnail}
             alt="no img"
             width={120}
             height={150}
@@ -22,9 +51,9 @@ export function ItemCard({ product }) {
         <span>
           <h2>{product.title}</h2>
           <p>{product.price}€</p>
-          <form>
+          <form onSubmit={handleSubmit}>
             <span>
-              <button>
+              <button type="button" onClick={() => decreaseCounter()}>
                 <Icon.SmallMinus />
               </button>
               <input
@@ -32,20 +61,16 @@ export function ItemCard({ product }) {
                 min="0"
                 max="99"
                 maxLength="2"
-                defaultValue={0}
-                placeholder={0}
-                onKeyDown={(e) => {
-                  if (e.target.value.length >= 2) {
-                    e.target.value = 0;
-                  }
-                }}
+                placeholder="0"
+                value={counter}
+                onChange={onChange}
               ></input>
-              <button>
+              <button type="button" onClick={() => increaseCounter()}>
                 <Icon.SmallPlus />
               </button>
             </span>
             <span>
-              <button>
+              <button type="submit">
                 <Icon.SmallCart />
               </button>
             </span>
