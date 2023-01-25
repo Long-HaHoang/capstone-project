0bdb73f8ef9a1364026585dff9dcd76a55a65b7e
LongHaHoang <longhahoang1992@gmail.com>
2023-01-25 15:20:05 +0100

    FIX: * import to single import

--- components/ShopHeader/ShopHeader.js
+++ components/ShopHeader/ShopHeader.js
@@ -3,14 +3,14 @@ import {
   StyledH1,
 } from "@/components/ShopHeader/ShopHeader.styled";
 
-import * as Icon from "@/components/Icons";
+import { SmallCart } from "@/components/Icons";
 
 export function ShopHeader({ cartTotal }) {
   return (
     <StyledHeader>
       <StyledH1>Shop Name</StyledH1>
       <p>
-        <Icon.SmallCart />
+        <SmallCart />
         <span>{cartTotal}</span>
       </p>
     </StyledHeader>
