07aac48e4ecb0db845a7f9e64d9bac82cf9bd017
LongHaHoang <longhahoang1992@gmail.com>
2023-01-28 15:44:43 +0100

    ADD db helper functions

---
+++ helpers/db.js
@@ -0,0 +1,36 @@
+import mongoose, { model, models, mongo, Schema } from "mongoose";
+import crypto from "crypto";
+
+const URI = `mongodb+srv://longha:${process.env.MONGODB_PASSWORD}@cluster0.oe4imt0.mongodb.net/capstone?retryWrites=true&w=majority`;
+
+const productSchema = new Schema(
+  {
+    id: String,
+    title: String,
+    description: String,
+    price: Number,
+    discountPercentage: mongoose.Decimal128,
+    rating: mongoose.Decimal128,
+    stock: Number,
+    brand: String,
+    category: String,
+    thumbnail: String,
+    images: [String],
+  },
+  { collection: "products" }
+);
+
+const Product = models.Product || model("Product", productSchema);
+
+async function connectDatabase() {
+  await mongoose.connect(URI);
+}
+
+async function getAllProducts() {
+  await connectDatabase();
+
+  const products = await Product.find({});
+  return products;
+}
+
+export { getAllProducts };
