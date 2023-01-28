f8ddc6650e6eba67a3a26e5b718daa1663fcc509
LongHaHoang <longhahoang1992@gmail.com>
2023-01-28 15:54:38 +0100

    fetch data from backend

--- components/Gallery/index.js
+++ components/Gallery/index.js
@@ -3,26 +3,36 @@ import styled from "styled-components";
 import useSWR from "swr";
 
 //Import internal resources
-import products from "@/products.json";
+// import products from "@/products.json";
 import ItemCard from "../ItemCard";
 
 // Import styles
 import * as Styled from "@/components/Gallery/Gallery.styles.js";
 
 export default function Gallery({ onhandleCartTotal }) {
+  const { data: products, isLoading, error } = useSWR("@/pages/api/products");
+
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
   return (
     <Styled.GallerySection>
-      <Styled.GalleryList>
-        {products.map((product) => {
-          return (
-            <ItemCard
-              key={product.id}
-              product={product}
-              onhandleCartTotal={onhandleCartTotal}
-            />
-          );
-        })}
-      </Styled.GalleryList>
+      {isLoading ? (
+        <li>Is Loading...</li>
+      ) : (
+        <Styled.GalleryList>
+          {products.map((product) => {
+            return (
+              <ItemCard
+                key={product.id}
+                product={product}
+                onhandleCartTotal={onhandleCartTotal}
+              />
+            );
+          })}
+        </Styled.GalleryList>
+      )}
     </Styled.GallerySection>
   );
 }
