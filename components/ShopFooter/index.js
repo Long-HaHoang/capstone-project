75bc87ccf96908c338b63a0263dba52ecb1924ab
LongHaHoang <longhahoang1992@gmail.com>
2023-01-31 12:17:03 +0100

    Add Link component

--- components/ShopFooter/index.js
+++ components/ShopFooter/index.js
@@ -1,16 +1,26 @@
+// Import of external resources
+import styled from "styled-components";
+// Import of internal resources
+import Link from "next/link";
 // Import of styled components
 import * as Styled from "@/components/ShopFooter/ShopFooter.styled.js";
-
 // Import of SVG Icons
 import { SmallCart } from "../Icons";
 
 export default function ShopFooter({ cartTotal }) {
   return (
     <Styled.ShopFooter>
-      <Styled.CartParagraph>
+      <Link href={"/"}>
         <SmallCart />
-        {cartTotal}
-      </Styled.CartParagraph>
+        <span>{cartTotal}</span>
+      </Link>
     </Styled.ShopFooter>
   );
 }
+
+const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
