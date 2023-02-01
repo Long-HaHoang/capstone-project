931cfff68cd97d709db7a28438bd9200f1f3ce28
LongHaHoang <longhahoang1992@gmail.com>
2023-02-01 22:30:49 +0100

    ADD styled componens for CartItemCard

---
+++ components/CartItemCard/CartItemCard.styled.js
@@ -0,0 +1,9 @@
+// Import external resources
+import Image from "next/image";
+// Import internal resources
+import styled from "styled-components";
+
+export const Thumbnail = styled(Image)`
+  border-radius: 8px;
+  object-fit: cover;
+`;
