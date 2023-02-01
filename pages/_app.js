72cfc6be82f0972d4d44f0bead59cce247a841f9
LongHaHoang <longhahoang1992@gmail.com>
2023-02-01 11:05:53 +0100

    ADD cartItem state

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
+// import external resources
+import { SWRConfig } from "swr";
+import { useImmer } from "use-immer";
+
 import GlobalStyle from "@/styles";
 import Head from "next/head";
-import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
+  const [cartItem, setCartItem] = useImmer([]);
+
+  function updateCartItem(newCart) {
+    setCartItem(newCart);
+  }
+
   return (
     <SWRConfig
       value={{
@@ -19,7 +28,11 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Capstone Project</title>
       </Head>
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        onUpdateCartItem={updateCartItem}
+        cartItem={cartItem}
+      />
     </SWRConfig>
   );
 }
