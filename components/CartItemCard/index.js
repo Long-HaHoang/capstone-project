46c94ff893827b21ea35a27025c94c7681ccc014
LongHaHoang <longhahoang1992@gmail.com>
2023-02-01 22:31:15 +0100

    Add CartItemCard component

---
+++ components/CartItemCard/index.js
@@ -0,0 +1,22 @@
+// Import internal resources
+
+// Import styled components
+import * as Styled from "@/components/CartItemCard/CartItemCard.styled.js";
+
+export default function CartItemCard({ eachItem }) {
+  return (
+    <li>
+      <article>
+        <Styled.Thumbnail
+          src={eachItem.thumbnail}
+          alt="no img avaible"
+          width={100}
+          height={100}
+        />
+        <p>{eachItem.title}</p>
+        <p>{eachItem.price}</p>
+        <p>{`Amount: ${eachItem.amount}`}</p>
+      </article>
+    </li>
+  );
+}
