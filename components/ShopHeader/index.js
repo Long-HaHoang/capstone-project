357187069b40dd91e14e69f1162662cb3315eecb
LongHaHoang <longhahoang1992@gmail.com>
2023-02-01 09:22:39 +0100

    reverse last changes, remove footer move shopping cart back

--- components/ShopHeader/index.js
+++ components/ShopHeader/index.js
@@ -1,5 +1,12 @@
+// Import external resources
+import styled from "styled-components";
+
+// Import internal resources
+import Link from "next/link";
+
 // Import of styled components
 import * as Styled from "@/components/ShopHeader/ShopHeader.styled.js";
+
 // Import of SVG Icon Component
 import { SmallCart } from "@/components/Icons";
 
@@ -7,6 +14,17 @@ export default function ShopHeader({ cartTotal }) {
   return (
     <Styled.ShopHeader>
       <Styled.ShopH1>Shop Name</Styled.ShopH1>
+      <Link href={"/CartOverview"}>
+        <SmallCart />
+        <span>{cartTotal}</span>
+      </Link>
     </Styled.ShopHeader>
   );
 }
+
+const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ pages/CartOverview/index.js
@@ -0,0 +1,11 @@
+//import internal resources
+import Link from "next/link";
+
+export default function CartOverviewPage() {
+  return (
+    <>
+      <Link href={"/"}>back</Link>
+      <h1>Hello this is the overpage</h1>
+    </>
+  );
+}
