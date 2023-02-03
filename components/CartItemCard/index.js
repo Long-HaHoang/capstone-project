7d85af926b5ff47d12afbd19ff5215cc32ac452a
LongHaHoang <longhahoang1992@gmail.com>
2023-02-03 16:04:57 +0100

    ADD delete button

--- components/CartItemCard/index.js
+++ components/CartItemCard/index.js
@@ -3,7 +3,7 @@ import { formatNumberToDeCurrency } from "@/helpers/formatNumberToCurrency";
 // Import styled components
 import * as Styled from "@/components/CartItemCard/CartItemCard.styled.js";
 
-export default function CartItemCard({ cartItem }) {
+export default function CartItemCard({ cartItem, onDeleteItem }) {
   return (
     <Styled.CartItemArticle>
       <Styled.Thumbnail
@@ -15,6 +15,9 @@ export default function CartItemCard({ cartItem }) {
       <p>{cartItem.title}</p>
       <p>{formatNumberToDeCurrency(cartItem.price)}</p>
       <p>{`Amount: ${cartItem.amount}`}</p>
+      <button type="button" onClick={() => onDeleteItem(cartItem.id)}>
+        Delete
+      </button>
     </Styled.CartItemArticle>
   );
 }
