4ad979bc5d7c02448123e7d9cbb433b5e0ccfb41
LongHaHoang <longhahoang1992@gmail.com>
2023-02-05 16:11:58 +0100

    ADD some aria-labels to icons

--- components/Icons/SmallMinus.js
+++ components/Icons/SmallMinus.js
@@ -4,6 +4,7 @@ import { StyledButtonSVG } from "./Icons.styled";
 export function SmallMinus() {
   return (
     <StyledButtonSVG
+      aria-label="minus icon"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
--- components/Icons/SmallPlus.js
+++ components/Icons/SmallPlus.js
@@ -4,6 +4,7 @@ import { StyledButtonSVG } from "./Icons.styled";
 export function SmallPlus() {
   return (
     <StyledButtonSVG
+      aria-label="plus icon"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
