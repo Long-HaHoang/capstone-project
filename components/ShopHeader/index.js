f971ad95991bdade6e743beb3a99e4d4a4f4f10c
LongHaHoang <longhahoang1992@gmail.com>
2023-02-09 09:34:45 +0100

    Refactor ShopHeader component

--- components/ShopHeader/index.js
+++ components/ShopHeader/index.js
@@ -4,28 +4,65 @@ import styled from "styled-components";
 // Import internal resources
 import Link from "next/link";
 
-// Import of styled components
-import * as Styled from "@/components/ShopHeader/ShopHeader.styled.js";
-
 // Import of SVG Icon Component
 import { SmallCart } from "@/components/Icons";
 
 export default function ShopHeader({ cartTotal }) {
   return (
-    <Styled.ShopHeader>
-      <Styled.ShopHeaderLink href={"/"}>
-        <Styled.ShopH1>Shop Name</Styled.ShopH1>
-      </Styled.ShopHeaderLink>
+    <StyledShopHeader>
+      <StyledShopHeaderLink href={"/"}>
+        <StyledShopH1>Shop Name</StyledShopH1>
+      </StyledShopHeaderLink>
 
-      <Styled.ShoppingCartLink
-        aria-label="Checkout link"
-        href={"/ShoppingCart"}
-      >
+      <StyledShoppingCartLink aria-label="Checkout link" href={"/ShoppingCart"}>
         <SmallCart />
         {cartTotal && (
-          <Styled.ShoppingCartCounter>{cartTotal}</Styled.ShoppingCartCounter>
+          <StyledShoppingCartCounter>{cartTotal}</StyledShoppingCartCounter>
         )}
-      </Styled.ShoppingCartLink>
-    </Styled.ShopHeader>
+      </StyledShoppingCartLink>
+    </StyledShopHeader>
   );
 }
+
+// Styled components
+const StyledShopHeader = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 3;
+
+  width: 100%;
+  height: 60px;
+  padding: 10px 25px;
+
+  background-color: var(--secondary-color);
+  color: var(--primary-color);
+  border-bottom: solid lightgray;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledShopH1 = styled.h1`
+  margin: 0;
+  width: 80%;
+`;
+
+const StyledShoppingCartLink = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  color: inherit;
+  text-decoration: inherit;
+`;
+
+const StyledShopHeaderLink = styled(Link)`
+  width: 100%;
+`;
+
+const StyledShoppingCartCounter = styled.span`
+  font-size: 18px;
+`;
