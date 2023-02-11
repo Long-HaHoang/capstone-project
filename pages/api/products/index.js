4795fe0dec66859e6633de44a534d2bcf2a59d34
LongHaHoang <longhahoang1992@gmail.com>
2023-02-12 00:06:07 +0100

    ADD CRUD post method

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,4 +1,4 @@
-import { getAllProducts } from "@/helpers/db";
+import { createProduct, getAllProducts } from "@/helpers/db";
 
 export default async function handler(request, response) {
   switch (request.method) {
@@ -7,11 +7,16 @@ export default async function handler(request, response) {
       response.status(200).json(products);
       break;
     }
+    case "POST": {
+      const product = JSON.parse(request.body);
+      const createdProduct = await createProduct(place);
+      response.status(201).json(createdProduct);
+    }
 
     default: {
       response
         .status(405)
-        .setHeader("Allow", "GET")
+        .setHeader("Allow", "GET,POST")
         .json({
           message: `Request method ${request.method} is not allowed on ${request.url}`,
         });
