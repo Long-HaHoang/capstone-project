ff0f8259e72449b50b9148834dcb444d31ddc07a
LongHaHoang <longhahoang1992@gmail.com>
2023-02-14 16:18:55 +0100

    ADD meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,10 @@ export default function App({ Component, pageProps }) {
     >
       <GlobalStyle />
       <Head>
-        <title>Capstone Project</title>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Capstone Store</title>
       </Head>
       <Layout>
         <Component {...pageProps} />
