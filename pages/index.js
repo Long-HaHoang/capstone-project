463e953203fb5885dc71e1c0982d7f357e924f1a
Long Ha-Hoang <github@ha.mozmail.com>
2023-02-22 23:43:32 +0100

    add searchbar div

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
+import styled from "styled-components";
+
 import Gallery from "@/components/Gallery";
 import HeroSection from "@/components/HeroSection";
 
 export default function HomePage() {
   return (
     <>
+      <StyledSearchBar>
+        <input type="text" placeholder="Search item..." />
+      </StyledSearchBar>
       <HeroSection />
       <Gallery />
     </>
   );
 }
+
+const StyledSearchBar = styled.div`
+  height: 60px;
+  text-align: center;
+  background-color: var(--secondary-color);
+  color: var(--primary-color);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
